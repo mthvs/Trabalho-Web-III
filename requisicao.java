180ba189ff669cd1d6141be1f40d2fd9fee08e2f
Matheus Henrique dos Santos Moreira <49769799+mthvs@users.noreply.github.com>
2021-09-13 20:44:21 -0300

    Update and rename AppServelet.java to requisicao.java

--- requisicao.java
+++ requisicao.java
@@ -9,14 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/requisicao")
-public class AppServelet extends HttpServlet {
+public class AppServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.getWriter().print("O Método é: " + req.getMethod());
-		resp.getWriter().print("<p> A URI do pedido é: " + req.getRequestURI() +"</p>");
-		resp.getWriter().print("<p> O Protocolo é: " + req.getProtocol()+"</p>");
+		resp.getWriter().print("O MÃ©todo Ã©: " + req.getMethod());
+		resp.getWriter().print("<p> A URI do pedido Ã©: " + req.getRequestURI() +"</p>");
+		resp.getWriter().print("<p> O Protocolo Ã©: " + req.getProtocol()+"</p>");
 		
 	}
 }
