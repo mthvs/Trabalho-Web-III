5bde76fe9af6ef6a2c05fe844b592d5c4266a4a0
Matheus Henrique dos Santos Moreira <49769799+mthvs@users.noreply.github.com>
2021-09-13 20:41:08 -0300

    Update and rename appServelet.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("")
-public class appServelet extends HttpServlet {
+public class appServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
